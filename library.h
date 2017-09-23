1d93c391093b3cb9bafd370a80b53ae87350a292
RapidTheNerd <george.marr.cobc@gmail.com>
2017-09-23 23:12:36 +0100

    removed unused shit

--- library.h
+++
@@ -1,6 +0,0 @@
-#ifndef RFID_LIBRARY_H
-#define RFID_LIBRARY_H
-
-void hello(void);
-
-#endif
--- spi.h
+++ spi.h
@@ -7,7 +7,7 @@
 
 #include SPI_H
 
-void spi_init()
+void spi_init();
         uint8_t spi_transmit(uint8_t data);;
 
 #define ENABLE (SPI_PORT &=(~(1<<SPI_SS)))
